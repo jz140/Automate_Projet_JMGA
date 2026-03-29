4911e0bf799b5675406edbaa9845dfd3e26474ca
jackyzhang <jacky.zhang@efrei.net>
2026-03-29 14:40:52 +0200

    rrr

---
+++ fonction.py
@@ -0,0 +1,43 @@
+#fonction
+
+def lire_automate_sur_fichier():
+    pass
+
+def afficher_automate():
+    pass
+
+def non_standard():
+    pass
+
+def standardisation():
+    pass
+
+def est_un_automate_deterministe():
+    pass
+
+def est_un_automate_complet():
+    pass
+
+def completion():
+    pass
+
+def determinisation_et_completion_automate():
+    pass
+
+def afficher_automate_deterministe_complet():
+    pass
+
+def minimisation():
+    pass
+
+def afficher_automate_minimal():
+    pass
+
+def lire_mot():
+    pass
+
+def reconnaitre_mot():
+    pass
+
+def automate_complementaire():
+    pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from fonction import *
+
+def main():
+
