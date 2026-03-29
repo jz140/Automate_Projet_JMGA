9df5bc7bf8514787422aaad72658c0ece6d0309b
jackyzhang <jacky.zhang@efrei.net>
2026-03-29 14:46:09 +0200

    rrr

--- fonction.py
+++ fonction.py
@@ -1,43 +1,43 @@
 #fonction
 
-def lire_automate_sur_fichier():
+def lire_automate_sur_fichier(nom_du_fichier):
     pass
 
-def afficher_automate():
+def afficher_automate(automate):
     pass
 
-def non_standard():
+def non_standard(automate):
     pass
 
-def standardisation():
+def standardisation(automate):
     pass
 
-def est_un_automate_deterministe():
+def est_un_automate_deterministe(automate):
     pass
 
-def est_un_automate_complet():
+def est_un_automate_complet(automate):
     pass
 
-def completion():
+def completion(automate):
     pass
 
-def determinisation_et_completion_automate():
+def determinisation_et_completion_automate(automate):
     pass
 
-def afficher_automate_deterministe_complet():
+def afficher_automate_deterministe_complet(automate):
     pass
 
-def minimisation():
+def minimisation(automate):
     pass
 
-def afficher_automate_minimal():
+def afficher_automate_minimal(automate):
     pass
 
-def lire_mot():
+def lire_mot(mot):
     pass
 
-def reconnaitre_mot():
+def reconnaitre_mot(mot, A):
     pass
 
-def automate_complementaire():
+def automate_complementaire(automate):
     pass
