cda30cdfcb119c365ad85ac1189cf72588914db1
jackyzhang <jacky.zhang@efrei.net>
2026-03-29 15:19:41 +0200

    rrr

--- fonction.py
+++ fonction.py
@@ -1,11 +1,94 @@
-#fonction
+import copy
 
+class Automate:
+    def __init__(self):
+        self.alphabet = []
+        self.etats = []
+        self.etats_initiaux = []
+        self.etats_finaux = []
+        self.transitions = []
 def lire_automate_sur_fichier(nom_du_fichier):
-    pass
+    automate = Automate()
 
+    f = open(nom_du_fichier, "r")
+    lignes = f.readlines()
+    f.close()
+
+    # alphabet
+    n = int(lignes[0])
+    for i in range(n):
+        automate.alphabet.append(chr(ord('a') + i))
+
+    # états
+    n = int(lignes[1])
+    for i in range(n):
+        automate.etats.append(i)
+
+    # états initiaux
+    parties = lignes[2].split()
+    for i in range(1, len(parties)):
+        automate.etats_initiaux.append(int(parties[i]))
+
+    # états finaux
+    parties = lignes[3].split()
+    for i in range(1, len(parties)):
+        automate.etats_finaux.append(int(parties[i]))
+
+    # transitions
+    n = int(lignes[4])
+    for i in range(n):
+        parties = lignes[5 + i].split()
+        depart = int(parties[0])
+        symbole = parties[1]
+        arrivee = int(parties[2])
+
+        automate.transitions.append((depart, symbole, arrivee))
+
+    return automate
 def afficher_automate(automate):
-    pass
 
+    # afficher l'entête
+    print("   ", end="")
+    print(" ", end="")
+    for symbole in automate.alphabet:
+        print(symbole, end="   ")
+    print()
+
+    # pour chaque état
+    for etat in automate.etats:
+
+        # on affiche les entrées et les sorties
+        if etat in automate.etats_initiaux:
+            print("E ", end="")
+        elif etat in automate.etats_finaux:
+            print("S ", end="")
+        else:
+            print("  ", end="")
+
+        # numéro état
+        print(etat, end="   ")
+
+        # pour chaque symbole
+        for symbole in automate.alphabet:
+
+            arrivees = []
+
+            # chercher transitions
+            for t in automate.transitions:
+                if t[0] == etat and t[1] == symbole:
+                    arrivees.append(t[2])
+
+            # affichage
+            if len(arrivees) == 0:
+                print("--", end="  ")
+            else:
+                for i in range(len(arrivees)):
+                    print(arrivees[i], end="")
+                    if i < len(arrivees) - 1:
+                        print(",", end="")
+                print("  ", end="")
+
+        print()
 def non_standard(automate):
     pass
 
